6e94311a061e81a0f76883ad0912106e1f658bef
sanjshah2001 <sanj_shah2001@yahoo.com>
2017-09-17 19:45:31 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import csv
+from FormattedData import FormatData
+
+NewData = [FormatData("George", 65, True),
+           FormatData("Sally", 47, False),
+           FormatData("Doug", 52, True)]
+
+for Entry in NewData:
+   print(Entry)
+
+def SaveData(Filename = "", DataList = []):
+   with open(Filename,
+             "w") as csvfile:
+      DataWriter = csv.writer(
+         csvfile,
+         delimiter='\n',
+         quotechar=" ",
+         quoting=csv.QUOTE_NONNUMERIC)
+      DataWriter.writerow(DataList)
+      csvfile.close()
+      print("Data saved!")
+
+SaveData("TestFile.csv", NewData)
